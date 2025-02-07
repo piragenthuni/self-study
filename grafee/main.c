ff890f41583d9c44744fe2fc1fbc47f674a99fad
piragenthuni <piragenthuni@gmail.com>
2025-02-07 12:15:26 +0530

    git

---
+++ grafee/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[] = "piragenth";
+    char grade = 'A';
+    int characterAge = 35;
+    double dpa = 34.34;
+
+    printf("my name is %s\n",characterName);
+    printf("my age is %d\n", characterAge);
+    printf("I really liked the name %s\n",characterName);
+    printf("I also like being %d\n",characterAge);
+    return 0;
+}
