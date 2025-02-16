962d8262f863ae4b43bffe4db3e2249bc9854a23
piragenthuni <piragenthuni@gmail.com>
2025-02-16 10:41:26 +0530

    mail

--- grafee/main.c
+++ grafee/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
     char characterName[] = "piragenth";
     char grade = 'A';
     int characterAge = 35;
-    double dpa = 34.34;
+    double gpa = 34.34;
 
+    printf("%f  \n", pow(2, 3) );
+    printf("%f  \n", sqrt(144) );
+    printf("%f \n", ceil(34.12312312));
+    printf("%d \n" , 300);
+    printf("%s's favourite number is %f \n",characterName,5.323123123);
     printf("my name is %s\n",characterName);
     printf("my age is %d\n", characterAge);
     printf("I really liked the name %s\n",characterName);
